a830c62ccd2c69a347e27c0d07ce334b75ba8bec
Duke <50648115+Vafelz@users.noreply.github.com>
2020-06-07 15:50:57 -0400

    v0.0.1
    - Created 'hello.py'
    - Added docstring description for "The Hello Program"
    - Created hello world print statement

---
+++ SPRING 2020/OLRN1506 Introduction to Python Programming/Week 3 Writing a First Program/hello.py
@@ -0,0 +1,10 @@
+# This is a block comment
+
+"""
+The Hello Program
+
+This is a program that prints out "Hello World!".
+To use it, run 'python hello.py'.
+"""
+
+print('Hello world!') # This is an inline comment
