c81afa035fe01c255e45a86a49afd6b384133bb0
Duke <50648115+Vafelz@users.noreply.github.com>
2020-06-07 16:25:05 -0400

    Practice Activities - v0.1.0
    + Completed practice activity 1
    + Completed practice activity 2
    + Created week 4 documentation

---
+++ SPRING 2020/OLRN1506 Introduction to Python Programming/Week 3/Practice Activity #1/program.py
@@ -0,0 +1,13 @@
+"""
+Intro Program
+
+This is a python script for the first practice activity (non-graded).
+This includes a short introduction to myself including: name, occupation
+and short biography (2-3 lines).
+"""
+
+print('Name: Duke Do') # REQUIRED: Name
+print('Occupation: Student') # REQURIED: occupation
+print('I am currently living with my partner and our little pupper, Scout (Border/Aussie).')
+print('I like playing video games and dream of working in game or web dev in the future.')
+print('I also like to be active and travelling.')
---
+++ SPRING 2020/OLRN1506 Introduction to Python Programming/Week 3/Practice Activity #2/lettertoself.py
@@ -0,0 +1,17 @@
+"""
+Letter to Self Program
+
+A simple program with a letter to myself
+"""
+
+print('|--------------------------------------------------------------------------------|')
+print('|                                                                           #### |')
+print('|                                                                           #### |')
+print('|                                                                           #### |')
+print('|                                                                                |')
+print('|                                                                                |')
+print('|                                                  Duke Do                       |')
+print('|                                                  41 Ezra Avenue                |')
+print('|                                                  Waterloo, ON N2L3A9           |')
+print('|                                                                                |')
+print('|--------------------------------------------------------------------------------|')
