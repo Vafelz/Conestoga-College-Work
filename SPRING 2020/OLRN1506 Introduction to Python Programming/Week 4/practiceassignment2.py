95c32d3b653d1560403fb13c7bb49e919d8ce20d
Duke <50648115+Vafelz@users.noreply.github.com>
2020-06-07 16:59:09 -0400

    practiceassignment2 v0.2.1
    + Completed practiceassignment2

---
+++ SPRING 2020/OLRN1506 Introduction to Python Programming/Week 4/practiceassignment2.py
@@ -0,0 +1,45 @@
+"""
+WEEK 4: Practice Assignment #2
+"""
+
+# Printing string
+print("I will now count my chickens:")
+
+# Prints float of hens
+print("Hens", (25 + 30 / 6))
+
+# Prints int of roosters ('%')
+print("Roosters", (100 - 25 * 3 % 4))
+
+# Prints string
+print("Now I will count the eggs:")
+
+# Prints float of calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Prints string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Prints bool
+print(3 + 2 < 5 - 7)
+
+# Prints string + int sum
+print("What is 3 + 2?", (3 + 2))
+
+# Prints string + int difference
+print("What is 5 - 7?", (5 - 7))
+
+# Prints string
+print("Oh, that's why it's false.")
+
+# Prints string
+print("How about some more.")
+
+# Prints string + bool
+print("Is it greater?", (5 > -2))
+
+# Prints string + bool
+print("Is it greater or equal?", (5 >= -2))
+
+# Prints string + bool
+print("Is it less or equal?", (5 <= -2))
