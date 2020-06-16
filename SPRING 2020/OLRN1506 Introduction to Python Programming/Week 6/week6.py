5807ee2c15a7376bc9e28f5435f6941806697283
Duke <50648115+Vafelz@users.noreply.github.com>
2020-06-16 13:00:48 -0400

    Started week 6
    - Created week6.py

---
+++ SPRING 2020/OLRN1506 Introduction to Python Programming/Week 6/week6.py
@@ -0,0 +1,16 @@
+"""
+WEEK 5
+
+June 16-22
+
+MODULE OBJECTIVES
+- Identify the different string formatting and interpolation methods
+- Bind data to string templates
+- Print and format numbers
+- Print dates and times in various formats
+
+|----------------------------------------------------------------------|
+
+STRING FORMATTING IN PYTHON
+
+"""
